883e15cd4b5cccd42cd5f6275222623276087ed7
sist <sist@DESKTOP-499LE1F>
2020-04-07 10:44:42 +0900

    commit

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	 System.out.println("Hello world");
 	 System.out.println("Hello git");
+	 System.out.println("Hello Jaewon");
 
 	}
 
